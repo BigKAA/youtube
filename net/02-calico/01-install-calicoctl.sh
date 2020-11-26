4defcf70151d04fdcfa99bc01d6fc72727098623
Artur Kryukov <artur@kryukov.biz>
2020-11-26 17:05:55 +0300

    поправил скрипт

--- net/02-calico/01-install-calicoctl.sh
+++ net/02-calico/01-install-calicoctl.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #curl -Os -L https://github.com/projectcalico/calicoctl/releases/download/v3.16.5/calicoctl
-curl -Os -L https://github.com/projectcalico/calicoctl/releases/download/v3.17/calicoctl
+#curl -Os -L https://github.com/projectcalico/calicoctl/releases/download/v3.17/calicoctl
+curl -Os -L https://github.com/projectcalico/calicoctl/releases/download/v3.17.0/calicoctl
 chmod +x calicoctl
-mv calicoctl /usr/local/bin
+mv -f calicoctl /usr/local/bin
 mkdir /etc/calico
